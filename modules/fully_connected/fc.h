bdb4169cd261948bcb09a159ee0b8d9b0b0db63e
Felucco <matteo.mannis@libero.it>
2022-04-30 14:05:14 +0200

    Removed old modules folder

--- modules/fully_connected/fc.h
+++
@@ -1,5 +0,0 @@
-#include "headers/defines.h"
-#include "headers/activations.h"
-#include <hls_stream.h>
-
-void fc(hls::stream<float> &out, hls::stream<float> &in, float weight[FC_WEIGHTS_H][FC_WEIGHTS_W], float bias[FC_BIAS_SIZE]);
